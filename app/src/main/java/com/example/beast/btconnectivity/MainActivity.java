bb417ee0d1998a9c0d0cd823a45d76b59ae40933
unknown <aa.97gupta@gmail.com>
2019-05-25 22:20:16 +0530

    1

---
+++ app/src/main/java/com/example/beast/btconnectivity/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.beast.btconnectivity;
+
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothManager;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private BluetoothAdapter mBluetoothAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if (!getPackageManager().hasSystemFeature(PackageManager.FEATURE_BLUETOOTH_LE)) {
+            Toast.makeText(this, R.string.ble_not_supported, Toast.LENGTH_SHORT).show();
+            finish();
+        }
+
+        final BluetoothManager bluetoothManager =
+                (BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE);
+        mBluetoothAdapter = bluetoothManager.getAdapter();
+
+        if (mBluetoothAdapter == null || !mBluetoothAdapter.isEnabled()) {
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            int REQUEST_ENABLE_BT=1;
+            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+            Toast.makeText(this, "bluetooth is enable", Toast.LENGTH_SHORT).show();
+        }else{
+            Toast.makeText(this, "bluetooth is enable", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void bt(View view) {
+        startActivity(new Intent(MainActivity.this,DeviceScanActivity.class));
+    }
+}
